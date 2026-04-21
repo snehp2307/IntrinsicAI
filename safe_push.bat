b23a51138c58e8c66bc0b6f2d8ef47de716214d5
snehp2307 <snehpatel2307@gmail.com>
2026-04-22 00:43:38 +0530

    Cleaned up temporary deployment scripts

--- safe_push.bat
+++
@@ -1,7 +0,0 @@
-rmdir /s /q .git
-git clone https://github.com/snehp2307/IntrinsicAI.git temp_clone
-xcopy temp_clone\.git .git\ /E /H /C /I /Y
-rmdir /s /q temp_clone
-git add .
-git commit -m "Refactored valuation module, NIFTY 500 support, yfinance fallback"
-git push origin main
